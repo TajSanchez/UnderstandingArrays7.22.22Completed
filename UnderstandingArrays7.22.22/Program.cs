c9feab1ab42afca0d56041304ba6e948c8fe7b62
Taj <tajmarie92@gmail.com>
2022-07-22 21:25:31 -0500

    Add project files.

---
+++ UnderstandingArrays7.22.22/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace UnderstandingArrays7._22._22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////
+            //int number1 = 4;
+            //int number2 = 8;
+            //int number3 = 15;
+            //int number4 = 16;
+            //int number5 = 23;
+
+            //if (number1 == 16)
+            //{
+            //}
+            //else if (number2 == 16)
+            //{
+            //}
+            //else if (number3 == 16) 
+            //{ 
+            //}
+
+            //// int[] numbers = new int[5];
+
+            // numbers[0] = 4;
+            // numbers[1] = 8;
+            // numbers[2] = 15;
+            // numbers[3] = 16;
+            // numbers[4] = 23;
+            // //numbers[5] = 42;
+
+            // //Console.WriteLine(numbers[1]);
+            // Console.WriteLine(numbers.Length);
+
+            //int[] numbers = new int[] { 4, 8, 15, 16, 23, 42 };
+
+            //string[] names = new string[] { "Eddie", "Alex", "Michael", "David" };
+
+            //for (int i = 0; i < names.Length; i++)
+            //{
+            //    Console.WriteLine(names[i]);
+            //}
+
+            //foreach (string name in names)
+            //{
+            //    Console.WriteLine(name);
+            //}
+
+            string zig = "You can get what you want out of life" +  "if you help enough other people get what they want.";
+
+            char[] charArray = zig.ToCharArray();
+            Array.Reverse(charArray);
+
+            foreach (char zigChar in charArray)
+            {
+                Console.Write(zigChar);
+            }
+            
+        }
+    }
+}
